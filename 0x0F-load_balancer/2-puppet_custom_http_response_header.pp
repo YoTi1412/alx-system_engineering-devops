a5bd140a7ef6b33a7afd37c09d43aed6775e05ec
YoTi <yasserrafai5@gmail.com>
2023-10-03 03:51:09 +0100

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
-# custom http response header
+# custom HTTP header with Puppet
 
-sudo apt -y update
-sudo apt -y install nginx
-
-echo "Hello World!" | sudo tee /var/www/html/index.html
-new_str="server_name _;\n\trewrite ^\/redirect_me https:\/\/www.github.com\/amasin76 permanent;"
-sudo sed -i "s/server_name _;/$new_str/" /etc/nginx/sites-enabled/default
-
-echo "Ceci n'est pas une page" | sudo tee /var/www/html/404.html
-new_str="listen 80 default_server;\n\terror_page 404 \/404.html;\n\tlocation = \/404.html {\n\t\troot \/var\/www\/html;\n\t\tinternal;\n\t}"
-sudo sed -i "s/listen 80 default_server;/$new_str/" /etc/nginx/sites-enabled/default
-
-sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default
-
-sudo nginx -t
-sudo service nginx restart
+exec { 'update server':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell',
+}
+->
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
+}
+->
+file_line { 'add HTTP header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;'
+}
+->
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx']
+}
